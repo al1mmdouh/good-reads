e15823144e620d6c19b958c8a145b391c2dac884
al1mmdouh <alimmdouh340@gmail.com>
2023-06-06 00:41:55 +0300

    User controller and route created

---
+++ src/routes/user.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+
+const {
+  userCreateValidationRules,
+  userUpdateValidationRules,
+} = require("../models/user");
+
+const { validate } = require("../Middlewares/validation");
+
+const { authenticate, checkRole } = require("../middlewares/auth"); // TODO: paths alias import
+
+const user = Router();
+
+user
+  .route("/users")
+  .get(getAllUsers)
+  .post(
+    authenticate,
+    checkRole(["reader", "admin"]),
+    validate(userCreateValidationRules),
+    createUser
+  );
+
+user
+  .route("/users/:id")
+  .get(getUserById)
+  .put(
+    authenticate,
+    checkRole(["reader", "admin"]),
+    validate(userUpdateValidationRules),
+    updateUser
+  )
+  .delete(authenticate, checkRole(["reader", "admin"]), deleteUser);
+module.exports = user;
