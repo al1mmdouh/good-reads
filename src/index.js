2fb3692aee05cd1200cbca679c61ee00e809f4a1
al1mmdouh <alimmdouh340@gmail.com>
2023-06-05 23:48:36 +0300

    MVC Project structure, server & db initialize

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// REQUIRES
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// EXPRESS SERVER
+const app = express();
+
+// MW
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// DB SERVER RUN
+mongoose
+  .connect(`${process.env.MONGO_URI}`)
+  .then(() => {
+    console.log("DB Connected");
+    addAdmin();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("DB Connection Error." + err));
