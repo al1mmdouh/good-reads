2e24ba751e6b0430aa69b4ce125b88d30c5633f1
noura mamdouh <nouramamdouh25@gmail.com>
2023-06-06 05:36:45 -0700

    creating model,controller,routes for category & getting all books of same category

---
+++ src/routes/category.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+
+const {
+  getAllCategories,
+  getCategoryById,
+  deleteCategory,
+  createCategory,
+  updateCategory,
+} = require("../controllers/category");
+
+const { categoryValidationRules } = require("../models/category");
+
+const { validate } = require("../Middlewares/validation");
+
+const { authenticate, checkRole } = require("../Middlewares/auth");
+
+category = new Router();
+
+category
+  .route("/category")
+  .get(getAllCategories) //All can get categories
+  .post(
+    authenticate,
+    checkRole(["admin"]), //Admin only can create category
+    validate(categoryValidationRules),
+    createCategory
+  );
+
+category
+  .route("/category/:id")
+  .get(getCategoryById) //All can get category by id
+  .put(
+    authenticate,
+    checkRole(["admin"]), //Admin only can edit category
+    validate(categoryValidationRules),
+    updateCategory
+  )
+  .delete(
+    authenticate,
+     checkRole(["admin"]),
+      deleteCategory); //Admin only can delete category
+
+module.exports = category;
