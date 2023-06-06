d0a0721c8f09ff744d72962d0e96239319ef2d20
m-saleh96 <scyllies@gmail.com>
2023-06-06 04:40:40 +0300

    ref auth and category to books

--- src/models/book.js
+++ src/models/book.js
@@ -6,9 +6,20 @@ const bookSchema = new mongoose.Schema({
     name: { type: String, required: true },
     title: { type: String, required: true },
     desc: { type: String, required: true },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author",
+        required: true,
+      },
+      category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+      },
   });
   
   const Book = mongoose.model("Book", bookSchema); // books --> Book
+
 
   module.exports = {Book};
   
